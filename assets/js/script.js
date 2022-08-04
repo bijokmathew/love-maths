ac961acde8a2826ddb71dcf63c9b38c548bf9042
bijokmathew <bijo.nbr@hotmail.com>
2022-08-04 22:41:27 +0000

    added code to generate two randowm numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 /* Get button element and add event listener to them */
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-    for(let button of buttons){
-        button.addEventListener("click",function(){
-            if(this.getAttribute("data-type")=== "submit"){
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit")
             } else {
                 let gameType = this.getAttribute("data-type");
@@ -12,30 +12,44 @@ document.addEventListener("DOMContentLoaded",function(){
         })
     }
 })
-function runGame(){
+/**
+ * The maim game loop is called when the script is first loaded 
+ * and after that user answer has been processed
+ */
+function runGame() {
+    // create two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
+
+function displayAditionQuestion() {
 
 }
-function displayAditionQuestion(){
+
+function displaySubstrationQuestion() {
 
 }
-function displaySubstrationQuestion(){
-    
-}
-function displayMultiplicationQuestion(){
-    
-}
-function displayDivisionQuestion(){
-    
-}
-function checkAnswer(){
+
+function displayMultiplicationQuestion() {
 
 }
-function calculateCorrectAnswer(){
+
+function displayDivisionQuestion() {
 
 }
-function incrementScore(){
+
+function checkAnswer() {
 
 }
-function incrementWrongAnswer(){
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
 
 }
